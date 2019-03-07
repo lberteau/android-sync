62562fc6e65a4699eace934f44f57f57a81a3903
Laurent Berteau <laurent@berteau.name>
2019-03-07 17:33:33 +0100

    Adding more directories

--- backup-opo.sh
+++ backup-opo.sh
@@ -5,19 +5,22 @@ adb_sync_home=/home/lberteau/android-sync/adb-sync
 source_home=/sdcard
 target_home=/mnt/e/backup/android-phone
 
-process_dir=keepass
-${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
-
 process_dir=.torque
-${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
-
-process_dir=Ringtones
 ${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
 process_dir=DCIM
 ${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
+process_dir=keepass
+${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
+process_dir=Movies
+${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
 process_dir=Music
+${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
+process_dir=Pictures
 ${adb_sync_home}/adb-sync --time --delete --reverse ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
 process_dir=Ringtones
--- restore-backup-adb-sync.sh
+++ restore-backup-adb-sync.sh
@@ -5,17 +5,26 @@ adb_sync_home=/home/lberteau/android-sync/adb-sync
 source_home=/mnt/e/backup/android-phone
 target_home=/sdcard
 
+process_dir=.torque
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
+process_dir=DCIM
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
 process_dir=keepass
 ${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
-#process_dir=DCIM
-#${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+process_dir=Movies
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
-#process_dir=Music
-#${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+process_dir=Music
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
-#process_dir=Ringtones
-#${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+process_dir=Pictures
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
+
+process_dir=Ringtones
+${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
 
 process_dir=Android/data/com.bambuna.podcastaddict/files/podcast
 ${adb_sync_home}/adb-sync --delete ${source_home}/${process_dir}/ ${target_home}/${process_dir}
